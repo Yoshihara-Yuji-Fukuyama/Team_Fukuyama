c8bf242bd313051c225fa1146ca543d0696142c1
Nakamoto-Kouta-Fukuyama <21013112@anabuki-net.ne.jp>
2024-03-04 11:14:58 +0900

    add

--- GameCamp2024/GameProgramming/src/CApplication.cpp
+++ GameCamp2024/GameProgramming/src/CApplication.cpp
@@ -1,19 +1,143 @@
-#include "CApplication.h"
 #include "CRectangle.h"
+#include "CApplication.h"
+#define SOUND_BGM "res\\mario.wav" //BGM音声ファイル
+#define SOUND_OVER "res\\mdai.wav" //ゲームオーバー音声ファイル
+
+//static変数の定義
+CCharacterManager CApplication::mCharacterManager;
+
+CTexture CApplication::mTexture;
+
+CTaskManager CApplication::mTaskManager;
+
+//メソッドの定義
+CCharacterManager* CApplication::CharacterManager()
+{
+	return &mCharacterManager;
+}
+
+CTexture* CApplication::Texture()
+{
+	return &mTexture;
+}
+
+CTaskManager* CApplication::TaskManager()
+{
+	return &mTaskManager;
+}
 
 void CApplication::Start()
 {
-	mRectangle.Set(400.0f, 300.0f, 200.0f, 100.0f);
-	mTexture.Load("ge-mu.png");
+	//アクションゲームプログラム
+
+	//Sound
+	mSoundBgm.Load(SOUND_BGM);
+
+	mSoundOver.Load(SOUND_OVER);
+
+	mFont.Load("FontWhite.png", 1, 64);
+
+	mState = EState::ESTART;
+
+	mpGame = new CGame();
 }
+
+
 
 void CApplication::Update()
 {
-	mRectangle.Render();
-	mTexture.DrawImage(
-		mRectangle.X() - mRectangle.W(),
-		mRectangle.X() + mRectangle.W(),
-		mRectangle.X() - mRectangle.W(),
-		mRectangle.X() + mRectangle.W(),
-		0.0f, 1920.0f, 0.0f, 1080.0f);
+	//アクションゲームプログラム
+	switch (mState)
+	{
+
+	case EState::ESTART: //状態がスタート
+
+		mpGame->Start(); //スタート画面表示
+
+		//Enterキーが押されたら
+		if (mInput.Key(VK_RETURN))
+		{
+			//状態をプレイ中にする
+			mState = EState::EPLAY;
+
+			//BGMリピート再生
+			mSoundBgm.Repeat();
+		}
+
+		break;
+
+
+
+	case EState::EPLAY: //状態がプレイ
+		
+		mpGame->Update();
+
+		//ゲームクリアか判定
+		if (mpGame->IsClear())
+		{
+			//状態をゲームクリアにする
+			mState = EState::ECLEAR;
+		}
+
+		//ゲームオーバーか判定
+		if (mpGame->IsOver())
+		{
+			//状態をゲームオーバーにする
+			mState = EState::EOVER;
+
+			//BGMストップ
+			mSoundBgm.Stop();
+
+			//ゲームオーバー音再生
+			mSoundOver.Play();
+		}
+
+		break;
+
+
+
+	case EState::ECLEAR: //状態がクリア
+
+		//ゲームクリア処理
+		mpGame->Clear();
+
+		//エンターキー入力時
+		if (mInput.Key(VK_RETURN))
+		{
+			//ゲームのインスタンス削除
+			delete mpGame;
+
+			//ゲームのインスタンス生成
+			mpGame = new CGame();
+
+			//状態をスタートにする
+			mState = EState::ESTART;
+		}
+
+		break;
+
+
+
+
+	case EState::EOVER: //状態がゲームオーバー
+
+		//ゲームオーバー処理
+		mpGame->Over();
+
+		//エンターキー入力時
+		if (mInput.Key(VK_RETURN))
+		{
+			//ゲームのインスタンス削除
+			delete mpGame;
+
+			//ゲームのインスタンス生成
+			mpGame = new CGame();
+
+			//状態をスタートにする
+			mState = EState::ESTART;
+		}
+
+		break;
+	}
 }
+
--- GameCamp2024/GameProgramming/src/CApplication.h
+++ GameCamp2024/GameProgramming/src/CApplication.h
@@ -1,16 +1,66 @@
 #pragma once
 #include "CRectangle.h"
 #include "CTexture.h"
+#include "CCharacter.h"
+#include "CBullet.h"
+#include "CEnemy.h"
+#include "CPlayer.h"
+#include "CFont.h"
+#include "CInput.h"
+#include "CMiss.h"
+#include "CCharacterManager.h"
+#include "CGame.h"
+#include "CSound.h"
+#include "CTaskManager.h"
 
 class CApplication
 {
+
 public:
-	//最初に一度だけ実行するプログラム
 	void Start();
-	//繰り返し実行するプログラム
+
 	void Update();
 
+	enum class EState
+	{
+		ESTART,
+		EPLAY,
+		ECLEAR,
+		EOVER,
+	};
+	
+	//staticメソッドの宣言
+	static CCharacterManager* CharacterManager();
+
+	static CTexture* Texture();
+
+	static CTaskManager* TaskManager();
+
 private:
-	CRectangle mRectangle;
-	CTexture mTexture;
+	EState mState;
+
+	CGame* mpGame;
+
+	CPlayer* mpPlayer;
+
+	CEnemy* mpEnemy;
+
+	CBullet* mpBullet;
+
+	CMiss* mpMiss;
+
+	CFont mFont;
+
+	CInput mInput;
+
+	CSound mSoundBgm; //BGM
+
+	CSound mSoundOver; //ゲームオーバー音
+
+	//static変数の宣言
+	static CCharacterManager mCharacterManager;
+
+	static CTexture mTexture;
+
+	static CTaskManager mTaskManager;
 };
